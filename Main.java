ce7408eebfd2bf8f3f528d89ec52535e337c4d99
johansonfelix <48577361+johansonfelix@users.noreply.github.com>
2020-11-06 20:25:09 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,16 +9,16 @@ public class Main {
         BusinessLayer msgboard = new BusinessLayer();
 
         //System.out.println(msgboard.createPost("01","Hey","001",LocalDate.parse("2020-10-01")).toString());
-        msgboard.createPost("01","Hey","001",LocalDate.parse("2020-10-01"));
-        msgboard.createPost("02","Hey","002",LocalDate.parse("2020-11-01"));
-        msgboard.createPost("03","Hey","003",LocalDate.parse("2020-10-05"));
-        msgboard.createPost("04","Hey","004",LocalDate.parse("2020-10-01"));
-        msgboard.createPost("05","Hey","005",LocalDate.parse("2020-10-06"));
-        msgboard.createPost("06","Hey","006",LocalDate.parse("2020-04-01"));
-        msgboard.createPost("07","Hey","007",LocalDate.parse("2020-10-02"));
-        msgboard.createPost("08","Hey","008",LocalDate.parse("2020-11-02"));
-        msgboard.createPost("09","Hey","009",LocalDate.parse("2020-10-10"));
-        msgboard.createPost("10","Hey","010",LocalDate.parse("2020-10-09"));
+        msgboard.createPost("01","Hey","001",LocalDate.parse("2020-10-01"), null);
+        msgboard.createPost("02","Hey","002",LocalDate.parse("2020-11-01"), null);
+        msgboard.createPost("03","Hey","003",LocalDate.parse("2020-10-05"),null);
+        msgboard.createPost("04","Hey","004",LocalDate.parse("2020-10-01"), null);
+        msgboard.createPost("05","Hey","005",LocalDate.parse("2020-10-06"), null);
+        msgboard.createPost("06","Hey","006",LocalDate.parse("2020-04-01"), null);
+        msgboard.createPost("07","Hey","007",LocalDate.parse("2020-10-02"), null);
+        msgboard.createPost("08","Hey","008",LocalDate.parse("2020-11-02"), null);
+        msgboard.createPost("09","Hey","009",LocalDate.parse("2020-10-10"), null);
+        msgboard.createPost("10","Hey","010",LocalDate.parse("2020-10-09"),null);
         msgboard.deletePost("007","07");
         System.out.println(msgboard.viewRecentPosts());
     }

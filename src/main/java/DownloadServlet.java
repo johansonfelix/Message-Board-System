3388213416562f9d315205e6671dc4fd1aa166f2
Antamar <arturojb98@gmail.com>
2020-11-19 23:28:22 +0100

    Changed DownloadServlet implementation

--- src/main/java/DownloadServlet.java
+++ src/main/java/DownloadServlet.java
@@ -8,7 +8,9 @@ import javax.servlet.ServletContext;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.OutputStream;
-import com.company.*;
+import java.sql.SQLException;
+
+import BusinessLayer.*;
 
 @WebServlet(name = "/DownloadServlet")
 public class DownloadServlet extends HttpServlet {
@@ -17,52 +19,41 @@ public class DownloadServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //This should be a reference to the main object or we should use a static method
-        BusinessLayer temp = new BusinessLayer();
+        MessageBoardModel mbm = MessageBoardModel.MessageBoardModel();
 
         /*
         if (user == null){
             //Error message
         }*/
-
-        //Read input
-        //String filePath = "C:\\Users\\Arturo\\Desktop\\RandomImages\\Guti.png";
-        //filePath = Post.getAttDestFilePath();
-        //File downloadFile = new File(filePath);
-        //FileInputStream inStream = new FileInputStream(downloadFile);
-        String PostID = request.getParameter("PostID");
+        int PostID = Integer.parseInt(request.getParameter("PostID"));
         String AttachmentD = request.getParameter("AttachmentID");
-        FileInputStream inStream = temp.getStream(PostID);
-
-        //Obtain servlet context
-        ServletContext context = getServletContext();
-
-        //Get MIME type
-        //String mimeType = context.getMimeType(filePath);
-        String mimeType = context.getMimeType(temp.getMIME(PostID));
+        byte[] attArray = new byte[0];
+        String mimeType = null;
+        String fileName = null;
+        //Get
+        try {
+            attArray = mbm.getAttachment(PostID);
+            mimeType = mbm.getAttachmentMIME(PostID);
+            fileName = mbm.getAttachmentName(PostID);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        //Set default mime type
         if (mimeType == null){
             mimeType = "application/octet-stream";
         }
-        System.out.print("MIME type: "+mimeType);
 
         //Modify response
         response.setContentType(mimeType);
 
         //Force download
         String headerKey = "Content-Disposition";
-        //String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
-        String headerValue = String.format("attachment; filename=\"%s\"", temp.getN(PostID));
+        String headerValue = String.format("attachment; filename=\"%s\"", fileName);
         response.setHeader(headerKey, headerValue);
 
         //Obtain response outStream
         OutputStream outStream = response.getOutputStream();
-
-        byte[] buffer = new byte[4096];
-        int bytesRead  = -1;
-        while ((bytesRead = inStream.read(buffer))!= -1){
-            outStream.write(buffer, 0, bytesRead);
-        }
-        inStream.close();
+        outStream.write(attArray);
         outStream.close();
 
         //Avoid caching
